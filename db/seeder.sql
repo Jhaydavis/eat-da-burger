08a59c4b810f0dbf38f4c12a726dd924be559348
Jhaydavis <Jhaydavis@mac.com>
2017-11-03 12:50:20 -0500

    Deploying Burger App

---
+++ db/seeder.sql
@@ -0,0 +1,8 @@
+USE `burgers_db`;
+
+-- insert 3 burgers
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Gumbo Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Shrimp Loaf', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Oyster Pounder', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Bayou Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('French Quarter Pounder', false, CURRENT_TIMESTAMP);
---
+++ routes/burgers_controller.js
@@ -0,0 +1,36 @@
+var db = require("../models");
+var path = require("path");
+
+module.exports = function (app) {
+
+    app.get("/index", function (req, res) {
+        db.Burger.findAll({
+            order: ['burger_name']
+        }).then(function (data) { // get all records, ordered by name
+            var hbsObject = {
+                burgers: data
+            };
+            res.render("index", hbsObject); // send to handlebars
+        });
+    });
+
+    app.post("/", function (req, res) {
+        db.Burger.create({
+            burger_name: req.body.burger_name
+        }).then(function () {
+            res.redirect("/index");
+        });
+    });
+
+    app.put("/:id", function (req, res) {
+        db.Burger.update({
+            devoured: true
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(function () {
+            res.redirect("/index"); // re-render html
+        });
+    });
+};
